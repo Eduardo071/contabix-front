c359095b57960420c9e446b5a0315f9a415f91fd
Samuel Vitor <samuelvitor668@gmail.com>
2024-10-31 20:00:16 -0300

    feat(Home): começando card de Boas vindas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ComponentsModule } from '../../components/components.module';
 import { SidenavComponent } from '../../components/sidenav/sidenav.component';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { AgendaAsideComponent } from '../../components/agenda-aside/agenda-aside.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, ComponentsModule, MatIconModule, AgendaAsideComponent],
+  imports: [RouterModule, ComponentsModule, MatIconModule, AgendaAsideComponent, MatCardModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
