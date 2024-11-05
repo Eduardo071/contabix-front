5a1499a4c90a7364f80f472ab9a57ed0b3692abd
Eduardo <ti22mau@gmail.com>
2024-11-05 18:27:11 -0300

    feat(solicitation): Criando componente de nova solicitação

---
+++ src/app/pages/solicitation/solicitation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitation',
+  standalone: true,
+  imports: [],
+  templateUrl: './solicitation.component.html',
+  styleUrl: './solicitation.component.scss'
+})
+export class SolicitationComponent {
+
+}
