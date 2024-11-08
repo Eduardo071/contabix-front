a5607c89ec56d2be3c028ed0e5cda7148c0b898d
Eduardo <ti22mau@gmail.com>
2024-11-07 22:36:57 -0300

    fix(env): consertando dinamicidade do serviço de calendar

--- src/app/services/agenda-controller.service.ts
+++ src/app/services/agenda-controller.service.ts
@@ -1,22 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { EnvironmentService } from '../shared/services/environment.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AgendaControllerService {
 
+  hostUrl: string = this.environmentService.getApiUrl() + '/calendar';
+
   constructor(
+    private readonly environmentService: EnvironmentService,
     private readonly http: HttpClient
-  ) { }
+  ) {}
 
   getEventByMonth(dateMonthYearFormatted: string): Observable<any> {
     const params = {
       dateMonthYearFormatted: dateMonthYearFormatted
     }
 
-    const url = 'https://contabix-api.onrender.com/contabix/calendar/getEventsByMonth';
+    const url = this.hostUrl + '/getEventsByMonth';
 
     return this.http.get(url, { params: params })
   }
