679bffc43c8af2af8f7184547d82f6e8f5cfb4a3
Samuel Vitor <samuelvitor668@gmail.com>
2025-05-06 01:11:58 -0300

    feat: ajusteDDDDDDD

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  API_URL: 'http://localhost:8080/contabix',
-  //  API_URL: 'https://contabix-api.onrender.com/contabix',
+ // API_URL: 'http://localhost:8080/contabix',
+    API_URL: 'https://contabix-api.onrender.com/contabix',
 };
